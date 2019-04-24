bb7653786a3726e2dddf952473e12c255a449d4f
Magnus Therning <magnus.therning@zimpler.com>
2019-04-24 11:08:13 +0200

    Upgrade ancient dependencies

--- project.clj
+++ project.clj
@@ -1,15 +1,15 @@
 (defproject ci-test-clj "0.1.0-SNAPSHOT"
   :description "The simplest of services based on Ring/Compojure/Component"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [compojure "1.6.0"]
-                 [ring/ring-defaults "0.3.1"]
-                 [ring/ring-jetty-adapter "1.6.3"]
-                 [com.stuartsierra/component "0.3.2"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [compojure "1.6.1"]
+                 [ring/ring-defaults "0.3.2"]
+                 [ring/ring-jetty-adapter "1.7.1"]
+                 [com.stuartsierra/component "0.4.0"]
                  [camel-snake-kebab "0.4.0"]
                  ;; [ring/ring-core "1.6.3"]
                  [ring/ring-json "0.5.0-beta1"]
-                 [com.stuartsierra/component "0.3.2"]
+                 [com.stuartsierra/component "0.4.0"]
 
                  ]
   :main ^:skip-aot play.core
