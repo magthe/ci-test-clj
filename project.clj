20d6231ebe8f3064cf9957b7ae6b3d93e6a8767b
Magnus Therning <magnus.therning@zimpler.com>
2019-04-24 11:34:48 +0200

    Added cloverage support to project

--- project.clj
+++ project.clj
@@ -10,8 +10,9 @@
                  [ring/ring-json "0.5.0-beta1"]
                  [com.stuartsierra/component "0.4.0"]]
   :main ^:skip-aot play.core
-
+  :cloverage {:fail-threshold 50}
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.11"]
                                   [com.stuartsierra/component.repl "0.2.0"]
-                                  [ring/ring-mock "0.3.2"]]}
+                                  [ring/ring-mock "0.3.2"]]
+                   :plugins [[lein-cloverage "1.1.1"]]}
              :uberjar {:aot :all}})
